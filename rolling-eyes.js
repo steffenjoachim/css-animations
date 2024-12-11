1034791fcc5ffff4067024a938be557ecb3789e0
Steffen Schanze <steffenjoachim.schanze@gmail.com>
2024-12-11 19:06:09 +0100

    header for rolling eyes

--- rolling-eyes.js
+++ rolling-eyes.js
@@ -6,7 +6,7 @@ function eyeball(event) {
         let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
         let y = eye.getBoundingClientRect().top + eye.clientHeight / 2;
         let radian = Math.atan2(event.pageY - y, event.pageX - x);
-        let rotation = (radian * (180 / Math.PI)) + 90; // Korrigierter Winkel
+        let rotation = (radian * (180 / Math.PI)) + 90; 
         eye.style.transform = "rotate(" + rotation + "deg)";
     });
 }
